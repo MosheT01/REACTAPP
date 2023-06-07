2f63c66dbd56f325fa458539714623978094f965
Mousa <mousatams@hotmail.com>
2023-06-07 21:30:52 +0300

    made so that buttons cant be pressed more than one time till we get a response from the server.

--- App/Screens/WelcomeScreen.js
+++ App/Screens/WelcomeScreen.js
@@ -1,51 +1,58 @@
 import React, { useState } from 'react';
 import { FIREBASE_AUTH, FIREBASE_DB } from './../../firebaseConfig';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword,sendPasswordResetEmail  } from "firebase/auth";
-import { getDoc, collection, doc, setDoc, query, where, getDocs} from 'firebase/firestore';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail } from "firebase/auth";
+import { getDoc, collection, doc, setDoc, query, where, getDocs } from 'firebase/firestore';
 import { View, TextInput, Button, StyleSheet, Text, Image, Alert } from 'react-native';
-
 
 const LoginScreen = ({ navigation }) => {
   const [email, setEmail] = useState('');
-  const [name, setName] = useState(''); 
+  const [name, setName] = useState('');
   const [password, setPassword] = useState('');
   const [layerCode, setLayerCode] = useState('');
   const [registerMode, setRegisterMode] = useState(false);
-  const [forgotPasswordMode, setForgotPasswordMode] = useState(false); // Add this line
+  const [forgotPasswordMode, setForgotPasswordMode] = useState(false);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false); // Add state for button disable/enable
 
-
-    const handleLogin = () => {
-      console.log("logging in...");
-      signInWithEmailAndPassword(FIREBASE_AUTH, email, password).then(async (userCredential) => {
+  const handleLogin = () => {
+    console.log("logging in...");
+    setIsButtonDisabled(true); // Disable button
+    signInWithEmailAndPassword(FIREBASE_AUTH, email, password)
+      .then(async (userCredential) => {
         console.log("logged in, checking user type");
         const docSnap = await getDoc(doc(FIREBASE_DB, 'users', userCredential.user.uid));
         if (docSnap.exists()) {
           const userID = String(docSnap.get('layer')); // user id
           const layers = userID.split('.');
           // TODO: testing - test if logging in to each type of user
-          if (layers.length == 4) navigation.navigate("Main", userID); // user is volunteer
-          else if (layers.length == 3) navigation.navigate("MainMS"); // user is school manager
-          else if (layers.length == 2) navigation.navigate("MainMT"); // user is regional manager
-          else if (layers.length == 1) navigation.navigate("MainM"); // user is admin
+          if (layers.length === 4) navigation.navigate("Main", userID); // user is volunteer
+          else if (layers.length === 3) navigation.navigate("MainMS"); // user is school manager
+          else if (layers.length === 2) navigation.navigate("MainMT"); // user is regional manager
+          else if (layers.length === 1) navigation.navigate("MainM"); // user is admin
           else console.log("valid user type not found");
         } else {
           console.log("ERROR: user data not found");
           Alert.alert('Error', 'User data not found. Please check your email and password.', [{ text: 'OK' }]);
-
         }
-      }).catch((error) => {
+      })
+      .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
         console.log("Error Code: ", errorCode, "\nError Message: ", errorMessage);
         Alert.alert('Error', 'User data not found. Please check your email and password.', [{ text: 'OK' }]);
-
+      })
+      .finally(() => {
+        setIsButtonDisabled(false); // Enable button
       });
   };
+
   const handleForgotPassword = () => {
     setForgotPasswordMode(true);
   };
+
   const handlePasswordReset = () => {
-    sendPasswordResetEmail(FIREBASE_AUTH, email).then(() =>  {
+    setIsButtonDisabled(true); // Disable button
+    sendPasswordResetEmail(FIREBASE_AUTH, email)
+      .then(() => {
         Alert.alert('Password Reset', 'A password reset email has been sent to your email address.', [
           { text: 'OK', onPress: handleBackToLogin },
         ]);
@@ -55,9 +62,11 @@ const LoginScreen = ({ navigation }) => {
         const errorMessage = error.message;
         console.log("Error Code: ", errorCode, "\nError Message: ", errorMessage);
         Alert.alert('Error', 'Failed to send password reset email. Please try again.', [{ text: 'OK' }]);
+      })
+      .finally(() => {
+        setIsButtonDisabled(false); // Enable button
       });
   };
-    
 
   const handleRegister = () => {
     setRegisterMode(true);
@@ -69,38 +78,44 @@ const LoginScreen = ({ navigation }) => {
   };
 
   const handleRegisterSubmit = async () => {
-    if (layerCode.split('.').length != 3){
+    if (layerCode.split('.').length !== 3) {
       console.log("invalid school code");
       Alert.alert('Error', 'Invalid School Code', [{ text: 'OK' }]);
       return;
     }
     console.log("registering user...");
+    setIsButtonDisabled(true); // Disable button
 
     // query check if the manager exists
     const q = query(collection(FIREBASE_DB, 'users'), where('layer', '==', layerCode));
     const managerSnapshot = await getDocs(q);
-    if (managerSnapshot.empty){
+    if (managerSnapshot.empty) {
       console.log("School not found")
       Alert.alert('Error', 'School not Found! Enter A valid School Code!', [{ text: 'OK' }]);
+      setIsButtonDisabled(false); // Enable button
       return;
     }
 
-
-    createUserWithEmailAndPassword(FIREBASE_AUTH, email, password).then((userCredential) => {
-      setDoc(doc(collection(FIREBASE_DB, 'users'), userCredential.user.uid), {
-        layer: layerCode + "." + userCredential.user.uid,
-        manager: layerCode,
-        name: name,
+    createUserWithEmailAndPassword(FIREBASE_AUTH, email, password)
+      .then((userCredential) => {
+        setDoc(doc(collection(FIREBASE_DB, 'users'), userCredential.user.uid), {
+          layer: layerCode + "." + userCredential.user.uid,
+          manager: layerCode,
+          name: name,
+        });
+        console.log(userCredential.user.email + "  registered successfully!");
+      })
+      .catch((error) => {
+        // error signing up user
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log("error code: " + errorCode + ": " + errorMessage);
+        Alert.alert('Registration Failed!', 'Error code: ' + errorCode + '\nError message: ' + errorMessage + '\n', [{ text: 'OK' }]);
+      })
+      .finally(() => {
+        setIsButtonDisabled(false); // Enable button
+        handleLogin();
       });
-      console.log(userCredential.user.email + "  registered successfully!");
-    }).catch((error) => {
-      // error signing up user
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log("error code: " + errorCode + ": " + errorMessage);
-      Alert.alert('Registration Failed!', 'Error code: ' + errorCode + '\nError message: ' + errorMessage + '\n', [{ text: 'OK' }]);
-    });
-    handleLogin();
   };
 
   if (forgotPasswordMode) {
@@ -117,7 +132,7 @@ const LoginScreen = ({ navigation }) => {
             onChangeText={text => setEmail(text)}
             value={email}
           />
-          <Button title="Reset Password" onPress={handlePasswordReset} />
+          <Button title="Reset Password" onPress={handlePasswordReset} disabled={isButtonDisabled} />
         </View>
         <Text style={styles.loginText}>
           Already have an account? <Text style={styles.loginLink} onPress={handleBackToLogin}>Login Here.</Text>
@@ -125,7 +140,7 @@ const LoginScreen = ({ navigation }) => {
       </View>
     );
   }
-  
+
   if (registerMode) {
     return (
       <View style={styles.container}>
@@ -145,19 +160,18 @@ const LoginScreen = ({ navigation }) => {
             value={password}
           />
           <TextInput
-          style={styles.input}
-          placeholder="Full Name"
-          onChangeText={text => setName(text)}
-          value={name}
+            style={styles.input}
+            placeholder="Full Name"
+            onChangeText={text => setName(text)}
+            value={name}
           />
-
           <TextInput
             style={styles.input}
             placeholder="School Code"
             onChangeText={text => setLayerCode(text)}
             value={layerCode}
           />
-          <Button title="Register" onPress={handleRegisterSubmit} />
+          <Button title="Register" onPress={handleRegisterSubmit} disabled={isButtonDisabled} />
         </View>
         <Text style={styles.registerText}>
           Already have an account? <Text style={styles.loginLink} onPress={handleBackToLogin}>Login here.</Text>
@@ -168,31 +182,30 @@ const LoginScreen = ({ navigation }) => {
 
   //TODO front Animation: loading...
   return (
-  <View style={styles.container}>
-    <Image source={require('../assets/logo.webp')} style={styles.logo} />
-    <View style={styles.form}>
-      <TextInput
-        style={styles.input}
-        placeholder="email"
-        onChangeText={text => setEmail(text)}
-        value={email}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry
-        onChangeText={text => setPassword(text)}
-        value={password}
-      />
-      <Button title="Log In" onPress={handleLogin} />
-      <Text style={styles.forgotPasswordLink} onPress={handleForgotPassword}>Forgot Password?</Text>
+    <View style={styles.container}>
+      <Image source={require('../assets/logo.webp')} style={styles.logo} />
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="email"
+          onChangeText={text => setEmail(text)}
+          value={email}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry
+          onChangeText={text => setPassword(text)}
+          value={password}
+        />
+        <Button title="Log In" onPress={handleLogin} disabled={isButtonDisabled} />
+        <Text style={styles.forgotPasswordLink} onPress={handleForgotPassword}>Forgot Password?</Text>
+      </View>
+      <Text style={styles.registerText}>
+        Don't have an account? <Text style={styles.registerLink} onPress={handleRegister}>Register here</Text>
+      </Text>
     </View>
-    <Text style={styles.registerText}>
-      Don't have an account? <Text style={styles.registerLink} onPress={handleRegister}>Register here</Text>
-    </Text>
-  </View>
-);
-
+  );
 };
 
 const styles = StyleSheet.create({
@@ -280,5 +293,4 @@ const styles = StyleSheet.create({
     textDecorationLine: 'underline',
   },
 });
-
 export default LoginScreen;
