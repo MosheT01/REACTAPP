7a20b5b86667cca6eae09a04db6cf239776b04d7
Mousa <mousatams@hotmail.com>
2023-06-10 12:25:07 +0300

    modified:   App/Screens/ManagerSchool/pdfMS.js from nitzan in progress

--- App/Screens/ManagerSchool/pdfMS.js
+++ App/Screens/ManagerSchool/pdfMS.js
@@ -1,24 +1,44 @@
 import React, { useState, useEffect } from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  FlatList,
-  TouchableOpacity,
-  Linking,
-  Modal,
-  TextInput,
-  Button,
+import {View,Text,StyleSheet,FlatList,TouchableOpacity,Linking,Modal,TextInput,Button,
 } from "react-native";
+import { Picker } from '@react-native-picker/picker'; // Updated import statement
 import Icon from "react-native-vector-icons/FontAwesome";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import * as DocumentPicker from 'expo-document-picker';
 
-function Pdf() {
+function Pdf({ route, navigation }) {
+  const vid = route.params;
+  const userID = String(vid); // user id
+  const layers = userID.split('.');
+  const [selectedFile, setSelectedFile] = useState(null);
+
+ 
+
+  const [userType, setUserType] = useState([]);
+
+  useEffect(() => {
+    if (layers.length === 4) {
+      setUserType("volunteer"); // user is volunteer
+    } else if (layers.length === 3) {
+      setUserType("schoolManager"); // user is school manager
+    } else if (layers.length === 2) {
+      setUserType("regionalManager"); // user is regional manager
+    } else if (layers.length === 1) {
+      setUserType("admin"); // user is admin
+    } else {
+      console.log("valid user type not found");
+    }
+  }, [layers]);
+  /////////////////////////////////////checking user type
+
   const [pdfList, setPdfList] = useState([]);
   const [modalVisible, setModalVisible] = useState(false);
   const [pdfName, setPdfName] = useState("");
   const [pdfDescription, setPdfDescription] = useState("");
   const [pdfUrl, setPdfUrl] = useState("");
+  const [selectedSchool, setSelectedSchool] = useState("");
+  const [filteredPdfList, setFilteredPdfList] = useState([]);
+  const schoolList = ["School A", "School B", "School C"];
 
   // Fetch PDF data from server or local storage
   useEffect(() => {
@@ -28,18 +48,32 @@ function Pdf() {
       {
         name: "PDF 1",
         description: "description 1",
+        school: "School A",
         pdfUrl:
           "https://leoro.org.il/wp-content/uploads/2021/10/%D7%A4%D7%A2%D7%99%D7%9C%D7%95%D7%AA-%D7%9C%D7%92%D7%99%D7%9C-%D7%94%D7%96%D7%94%D7%91-%D7%97%D7%93%D7%A9-1.pdf",
       },
       {
         name: "PDF 2",
         description: "description 2",
+        school: "School B",
         pdfUrl: "https://example.com/pdf2.pdf",
       },
       // ...rest of the PDF data
     ];
     setPdfList(pdfData);
+    setFilteredPdfList(pdfData);
   }, []);
+
+  //this function will be called whenever the selected school changes:
+  const filterPdfList = (selectedSchool) => {
+    if (selectedSchool === "") {
+      setFilteredPdfList(pdfList); // No filter applied, show all PDFs
+    } else {
+      const filteredList = pdfList.filter((pdf) => pdf.school === selectedSchool || pdf.school==="");
+      setFilteredPdfList(filteredList);
+    }
+  };
+  
 
   const downloadPdf = (url) => {
     Linking.openURL(url);
@@ -55,6 +89,7 @@ function Pdf() {
       setPdfList(updatedPdfList);
     };
 
+
     return (
       <View style={styles.pdfItem}>
         <TouchableOpacity onPress={() => downloadPdf(item.pdfUrl)}>
@@ -66,14 +101,17 @@ function Pdf() {
           <Text style={styles.pdfDescription}>{item.description}</Text>
         </View>
 
+        {userType === "schoolManager" && (
         <TouchableOpacity onPress={deletePdf}>
           <Icon name="trash" size={30} color="red" />
         </TouchableOpacity>
+      )}
       </View>
     );
   };
 
   const addPdf = () => {
+    
     // Validate the input fields
     if (!pdfName || !pdfDescription || !pdfUrl) {
       alert("Please fill in all fields");
@@ -85,6 +123,7 @@ function Pdf() {
       name: pdfName,
       description: pdfDescription,
       pdfUrl: pdfUrl,
+      school: selectedSchool,
     };
 
     // Update the PDF list
@@ -95,12 +134,49 @@ function Pdf() {
     setPdfDescription("");
     setPdfUrl("");
     setModalVisible(false);
+    filterPdfList(selectedSchool);
+
+  };
+  const handleFileSelection = async () => {
+    try {
+      const result = await DocumentPicker.getDocumentAsync({
+        type: Platform.OS === 'ios' ? 'application/pdf' : '/',
+      });
+
+      if (result.type === 'success') {
+        setSelectedFile(result.uri);
+        // Handle the selected file, e.g., upload it to a server
+      } else {
+        setSelectedFile(null);
+        // Handle cancellation or any other error
+      }
+    } catch (error) {
+      console.log('Error picking file:', error);
+    }
   };
 
   return (
     <View style={styles.container}>
+       {/* Filter section */}
+    {userType === "schoolManager" && (
+      <Picker
+        selectedValue={selectedSchool}
+        onValueChange={(itemValue) => {
+          setSelectedSchool(itemValue);
+          filterPdfList(itemValue);
+        }}
+        style={styles.filterPicker}
+      >
+        <Picker.Item label="All Schools" value="" />
+        {schoolList.map((school) => (
+          <Picker.Item label={school} value={school} key={school} />
+        ))}
+      </Picker>
+    )}
+
+    {/* PDF list */}
       <FlatList
-        data={pdfList}
+        data={filteredPdfList}
         renderItem={renderPdfItem}
         keyExtractor={(item) => item.name}
       />
@@ -126,7 +202,12 @@ function Pdf() {
               value={pdfDescription}
               onChangeText={(text) => setPdfDescription(text)}
             />
-
+              <View>
+              <Button title="Attach File" onPress={handleFileSelection} />
+              {selectedFile && (
+                <Text>Selected File: {selectedFile.name}</Text>
+              )}
+            </View>
             <TextInput
               style={styles.input}
               placeholder="PDF URL"
@@ -151,13 +232,15 @@ function Pdf() {
         </KeyboardAwareScrollView>
       </Modal>
 
-      {/* Button to open the modal */}
-      <TouchableOpacity
-        style={styles.addButton}
-        onPress={() => setModalVisible(true)}
-      >
-        <Icon name="plus" size={30} color="white" />
-      </TouchableOpacity>
+    {/* Button to open the modal (visible only for school managers) */}
+    {userType === "schoolManager" && (
+        <TouchableOpacity
+          style={styles.addButton}
+          onPress={() => setModalVisible(true)}
+        >
+          <Icon name="plus" size={30} color="white" />
+        </TouchableOpacity>
+      )}
     </View>
   );
 }
